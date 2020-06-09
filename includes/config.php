8852cffaef3ec5fc014746c31d5182e44b478c32
Haris Ainur Rozak <harisrozak@gmail.com>
2020-06-09 10:49:23 +0700

    minor update

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,11 @@
 <?php
 
 Class SGLTN_Config {
+	/**
+	 * Just a sample config variables
+	 * thought, on a real development, we don't need the dashicons strings to be pre-defined
+	 *
+	 */
 	protected $admin_product_icon = 'dashicons-image-filter';
 	protected $admin_order_icon = 'dashicons-cart';
 }
